b1cdd3b5e62ebbd75aad81f982378109a659d598
R.B <110852196+MRFAKE2002@users.noreply.github.com>
2024-10-25 11:05:28 +0330

    Make User and Profile models

--- back-end/userauths/models.py
+++ back-end/userauths/models.py
@@ -1,6 +1,8 @@
 # Django
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.dispatch import receiver
+from django.db.models.signals import pre_save, post_save
 
 # inja ma mikhaim az 'model' khod 'django' biaim ers bari konim ta 'model user' khodemun besazim. 
 class User(AbstractUser):
@@ -15,16 +17,17 @@ class User(AbstractUser):
     def __str__(self):
         return self.email
 
-    def save(self, *args, **kwargs):
-        email_username, email = self.email.split('@')
-        if self.fullname == "" or self.fullname == None:
-            self.fullname = email_username
 
-        super(User, self).save(*args, **kwargs)
+# dar inja miaim az 'signals' estefade mikonim va migim ghabl az 'save' shodan 'model' bia in 'function' ro seda bezan.
+@receiver(pre_save, sender=User)
+def set_fullname(sender, instance, **kwargs):
+    if not instance.fullname:
+        email_username, _ = instance.email.split('@')
+        instance.fullname = email_username
+
 
 class Profile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    fullname = models.CharField(max_length=100, blank=True, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='image/', default='default/default.jpg', null=True, blank=True)
     about = models.TextField(null=True, blank=True)
     gender = models.CharField(max_length=100, blank=True, null=True)
@@ -35,13 +38,29 @@ class Profile(models.Model):
     date_updated = models.DateField(auto_now=True)
     
     def __str__(self):
-        if self.fullname:
-            return self.fullname
-        else:
-            return self.user.fullname
+        # dar inja migim age 'fullname' dar 'model User' hast estefade kon age na az 'method get_full_name' estefade kon ke miad 'first_name va last_name' ro mizar.
+        return self.user.fullname or self.user.get_full_name()
 
-    def save(self, *args, **kwargs):
-        if self.fullname == "" or self.fullname == None:
-            self.fullname = self.user.fullname
+"""
+    dar in signal 'sender' mishe oun 'model' ke 'data' azash miad ke mishe 'User'.
+    'instance' mishe oun object az 'model' ke be tazegi 'zakhire' shode; ke inja mishe oun user ke be tazegi 'zakhire' ya 'update' shode.
+    'created' ham ye 'boolean' ke miporse object 'create' shode ya 'update' shode ke age 'True' bashe yani 'create' shode.
+    
+    kar in 'signal' sakht 'Profile' baraye har 'User' zaman 'create' ast.
+"""
+@receiver(post_save, sender=User)
+def create_profile_user(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
 
-        super(Profile, self).save(*args, **kwargs)
+"""
+    dar in signal 'sender' mishe oun 'model' ke 'data' azash miad ke mishe 'User'.
+    'instance' mishe oun object az 'model' ke 'update' shode.
+    
+    kar in 'signal' zakhire data 'update' shode dar 'profile' har 'user' ast. .
+"""
+@receiver(post_save, sender=User)
+def save_profile_user(sender, instance, **kwargs):
+    instance.profile.save()
+
+
