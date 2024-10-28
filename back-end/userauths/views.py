77bb6e50ff18977283ecb955b9fa1c4abd821794
R.B <110852196+MRFAKE2002@users.noreply.github.com>
2024-10-28 09:41:08 +0330

    Make Logon JWT token view

--- back-end/userauths/views.py
+++ back-end/userauths/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from .models import User, Profile
+from .serializer import JWTTokenSerializer, RegisterSerializer
+
+class JWTTokenView(TokenObtainPairView):
+  serializer_class = JWTTokenSerializer
