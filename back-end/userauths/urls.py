719c61eb78a1ab9e0833af6e747540fbc8cacdae
R.B <110852196+MRFAKE2002@users.noreply.github.com>
2024-10-29 20:34:04 +0330

    Add refresh token url for get new token

--- back-end/userauths/urls.py
+++ back-end/userauths/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from userauths import views as userauths_views
 
 
 urlpatterns = [
     # sakht 'url' baraye 'JWT token' vase 'login' user.
-    path("user/token/", userauths_views.JWTTokenView.as_view(), name="user_login_jwt_token"),
     path("user/register/", userauths_views.RegisterJWTView.as_view(), name="user_register_jwt"),
+    path("user/login/token/", userauths_views.JWTTokenView.as_view(), name="user_login_jwt_token"),
+    path("user/token/refresh/", TokenRefreshView.as_view(), name="user_refresh_jwt"),
 ]
