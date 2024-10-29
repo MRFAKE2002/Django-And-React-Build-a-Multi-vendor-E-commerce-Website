abc263cdef13eede2f5183f1d8fcfef7560376b6
R.B <110852196+MRFAKE2002@users.noreply.github.com>
2024-10-29 23:59:54 +0330

    Make PrivateRoute layout to check user login

---
+++ front-end/src/layout/PrivateRoute.js
@@ -0,0 +1,12 @@
+import { Navigate } from "react-router-dom";
+
+// store of zustand
+import { useAuthStore } from "../store/authStore"
+
+
+const PrivateRoute = ({children}) => {
+  const loggedIn = useAuthStore((state) => state.isLoggedIn)()
+  return loggedIn ? <>{children}</> : <Navigate to={'/login'} />
+}
+
+export default PrivateRoute
