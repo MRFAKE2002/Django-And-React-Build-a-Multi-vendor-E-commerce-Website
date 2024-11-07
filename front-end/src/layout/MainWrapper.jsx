43ef97f1df3911dafa34e0759ca65a22cb45ff3a
R.B <110852196+MRFAKE2002@users.noreply.github.com>
2024-11-07 18:08:17 +0330

    Explain MainWrapper process

--- front-end/src/layout/MainWrapper.jsx
+++ front-end/src/layout/MainWrapper.jsx
@@ -5,6 +5,20 @@ import { useEffect, useState } from "react";
 import { updateUserToken } from "../utils/auth";
 
 function MainWrapper({ children }) {
+  /*
+    nahve kar in component injuri ke shoma har moghe dakhel 'site' mishid miad 'check' mikone ke shoma 
+    dakhel 'cookie access token ya refresh token' dari ya na be ebarati mige 'login' hasti ya na hala:
+    
+    age 'login' nabud miad khali 'return' mikone.
+    
+    age 'login' bud miad aval 'tarikh expired token' ro 'check' mikone age 'tamum shode' bud miad 'refresh token' jadid misaze
+    age 'tarikh expired token tamum nashode' bud miad dobare hamun 'access va refresh token' ro ke az 'cookie' gerefte 
+    dobare be 'cookie' ezafe mikone va 'data user' ke dakhel oun 'token' hast ro dakhel 'zustand store' mizare ta bedunim
+    'isLoggedIn' dakhel 'zustand store True bar migardune'.
+
+    injuri age ma 'site' ro 'refresh' ham bokonim bazam in 'porose' etefagh miofte va moshakhas mishe 'isLoggedIn'
+    dakhel 'zustand store True bar migardune ya False'. 
+  */
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
