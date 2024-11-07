0963a2def1f936aec817630d0f4e2ae041db0652
R.B <110852196+MRFAKE2002@users.noreply.github.com>
2024-11-07 18:30:54 +0330

    Add Bootstrap template for ForgotPasswordEmailVerify page

--- front-end/src/views/auth/ForgotPasswordEmailVerify.jsx
+++ front-end/src/views/auth/ForgotPasswordEmailVerify.jsx
@@ -1,8 +1,8 @@
 // libraries
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 // API call Instance
-import axiosAPIInstance from '../../utils/axios'
+import axiosAPIInstance from "../../utils/axios";
 
 function ForgotPasswordEmailVerify() {
   /*
@@ -12,31 +12,80 @@ function ForgotPasswordEmailVerify() {
     mitune 'password' khodesh ro avaz kone va ma ham mitunim ba estefade az 'data' dakhel oun 'link' befahmim kodum 
     'user' oumade 'password' avaz kone va oun 'data' ro befrestim be 'back-end'.
   */
-  const [email, setEmail] = useState("")
+  const [email, setEmail] = useState("");
 
   const handelOnChange = (event) => {
-    setEmail(event.target.value)
-  }
+    setEmail(event.target.value);
+  };
 
   const handelOnclick = async (event) => {
     try {
-      // alan inja ma darim oun 'email' ro mifrestim be 'back-end' ta be 'user otp' bedim va befrestim 
+      // alan inja ma darim oun 'email' ro mifrestim be 'back-end' ta be 'user otp' bedim va befrestim
       // baraye 'change password'.
-      await axiosAPIInstance.get(`password-reset/${email}/`).then((response) => {
-        alert("An Email Has Been Sent To You.")
-      })
+      await axiosAPIInstance
+        .get(`password-reset/${email}/`)
+        .then((response) => {
+          alert("An Email Has Been Sent To You.");
+        });
     } catch (error) {
-      alert("Email Does Not Exist!")
+      alert("Email Does Not Exist!");
     }
-  }
+  };
   return (
     <>
-      <h1>Forgot password email verification</h1>
-      <input type="email" name="email" onChange={handelOnChange} />
-      <br/><br/>
-      <button type="submit" onClick={handelOnclick}>submit</button>
+      <main className="" style={{ marginBottom: 100, marginTop: 50 }}>
+        <div className="container">
+          {/* Section: Login form */}
+          <section className="">
+            <div className="row d-flex justify-content-center">
+              <div className="col-xl-5 col-md-8">
+                <div className="card rounded-5">
+                  <div className="card-body p-4">
+                    <h3 className="text-center">Forgot Password</h3>
+                    <br />
+
+                    <div className="tab-content">
+                      <div
+                        className="tab-pane fade show active"
+                        id="pills-login"
+                        role="tabpanel"
+                        aria-labelledby="tab-login"
+                      >
+                        <div>
+                          {/* Email input */}
+                          <div className="form-outline mb-4">
+                            <label className="form-label" htmlFor="Full Name">
+                              Email Address
+                            </label>
+                            <input
+                              onChange={handelOnChange}
+                              type="text"
+                              id="email"
+                              name="email"
+                              className="form-control"
+                            />
+                          </div>
+
+                          <div className="text-center">
+                            <button
+                              className="btn btn-primary w-100"
+                              onClick={handelOnclick}
+                            >
+                              Reset Password
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default ForgotPasswordEmailVerify
+export default ForgotPasswordEmailVerify;
