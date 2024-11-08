47c34c29f1c6befffc00ffe2ca5499dd345b0691
R.B <110852196+MRFAKE2002@users.noreply.github.com>
2024-11-08 14:13:31 +0330

    Make Category admin

--- back-end/store/admin.py
+++ back-end/store/admin.py
@@ -1,3 +1,22 @@
+# Django
 from django.contrib import admin
 
-# Register your models here.
+# My apps
+from .models import Category, Product
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["image", "title", "active"]
+
+    prepopulated_fields = {"slug": ["title"]}
+    
+    list_editable = ["active"]
+    
+    list_per_page = 10
+    
+    list_filter = ["active"]
+    
+    ordering = ["id", "title"]
+    
+    search_fields = ["title"]
+
