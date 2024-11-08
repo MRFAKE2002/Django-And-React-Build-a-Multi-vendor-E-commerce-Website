2e0c0e07430101ffdbfa303493fe1cf859d890e9
R.B <110852196+MRFAKE2002@users.noreply.github.com>
2024-11-08 13:51:07 +0330

    Make Vendor model

--- back-end/vendor/models.py
+++ back-end/vendor/models.py
@@ -1,3 +1,36 @@
+# Django
 from django.db import models
+from django.utils.text import slugify
+# My apps
+from userauths.models import User
 
-# Create your models here.
+
+class Vendor(models.Model):
+    user = models.OneToOneField(User, related_name="vendor", on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, help_text="Shop Name", unique=True)
+    slug = models.SlugField(unique=True, max_length=200)
+    description = models.TextField(blank=True, null=True)
+    mobile = models.CharField(
+        max_length=100, help_text="Shop Mobile Number", null=True, blank=True
+    )
+    image = models.ImageField(
+        upload_to="vendors", blank=True, null=True, default="vendor.jpg"
+    )
+    active = models.BooleanField(default=False)
+    datetime_created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = ""
+        managed = True
+        verbose_name = "vendor"
+        verbose_name_plural = "vendors"
+        ordering = ["datetime_created"]
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if self.slug == "" or self.slug is None:
+            self.slug = slugify(self.name)
+        super(Vendor, self).save(*args, **kwargs)
+
