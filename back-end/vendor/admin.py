2d567cb654a890131233b53625bf9f059fabd9bc
R.B <110852196+MRFAKE2002@users.noreply.github.com>
2024-11-08 14:11:12 +0330

    Make Vendor admin

--- back-end/vendor/admin.py
+++ back-end/vendor/admin.py
@@ -1,3 +1,26 @@
+# Django
 from django.contrib import admin
 
-# Register your models here.
+# My apps
+from .models import Vendor
+
+
+@admin.register(Vendor)
+class VendorAdmin(admin.ModelAdmin):
+    list_display = ["image", "user", "name", "mobile", "active"]
+
+    prepopulated_fields = {"slug": ["name"]}
+    
+    autocomplete_fields = ["user"]
+    
+    list_editable = ["active"]
+    
+    list_per_page = 10
+    
+    list_select_related = ["user"]
+    
+    list_filter = ["active", "datetime_created"]
+    
+    ordering = ["id", "datetime_created"]
+    
+    search_fields = ["user", "name", "mobile"]
