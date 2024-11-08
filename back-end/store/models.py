71de6babab3908ea6cf3e56602a35c82cfa97e19
R.B <110852196+MRFAKE2002@users.noreply.github.com>
2024-11-08 21:07:46 +0330

    Make Gallery model in store app

--- back-end/store/models.py
+++ back-end/store/models.py
@@ -9,13 +9,15 @@ from shortuuid.django_fields import ShortUUIDField
 from vendor.models import Vendor
 
 
-
 class Category(models.Model):
     title = models.CharField(max_length=150)
+    
     slug = models.SlugField(unique=True)
+    
     image = models.ImageField(
         upload_to="categories", blank=True, null=True, default="category.jpg"
     )
+    
     active = models.BooleanField(default=True)
 
     def __str__(self):
@@ -41,15 +43,30 @@ class Product(models.Model):
         ("published", "Published"),
     ]
 
+    pid = ShortUUIDField(unique=True, length=10, alphabet="abcdefgh12345")
+
     name = models.CharField(max_length=150)
+
     slug = models.SlugField(unique=True)
-    description = models.TextField()
-    category = models.ForeignKey(
-        Category, on_delete=models.SET_NULL, blank=True, null=True
+
+    vendor = models.ForeignKey(
+        Vendor, on_delete=models.CASCADE, related_name="products"
     )
+
+    description = models.TextField()
+
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name="products",
+    )
+
     image = models.ImageField(
         upload_to="products", blank=True, null=True, default="product.jpg"
     )
+
     # in mishe gheimat product 'ba takhfif'
     price = models.DecimalField(max_digits=8, decimal_places=2)
 
@@ -65,21 +82,18 @@ class Product(models.Model):
     # in baraye zamani ke 'stock_quantity' behse 0 yani product dakhel anbar nadarim.
     in_stock = models.BooleanField(default=True)
 
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="published")
+    status = models.CharField(
+        max_length=20, choices=STATUS_CHOICES, default="published"
+    )
 
     featured = models.BooleanField(default=False)
-    
+
     views = models.PositiveIntegerField(default=0)
-    
+
     rating = models.PositiveIntegerField(default=0)
-    
-    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
-    
-    pid = ShortUUIDField(unique=True, length=10, alphabet="abcdefgh12345")
-    
+
     datetime_created = models.DateTimeField(auto_now_add=True)
-    
-    
+
     def __str__(self):
         return self.name
 
@@ -94,4 +108,21 @@ class Product(models.Model):
         if self.slug == "" or self.slug is None:
             self.slug = slugify(self.name)
         super().save(*args, **kwargs)
+
+
+class Gallery(models.Model):
+    gid = ShortUUIDField(unique=True, length=10, alphabet="abcdefgh12345")
+    
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="galleries"
+    )
+
+    image = models.ImageField(upload_to="products", default="product.jpg")
+    
+    active = models.BooleanField(default=True)
+    
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.product.name
 
--- back-end/vendor/models.py
+++ back-end/vendor/models.py
@@ -1,22 +1,30 @@
 # Django
 from django.db import models
 from django.utils.text import slugify
+
 # My apps
 from userauths.models import User
 
 
 class Vendor(models.Model):
     user = models.OneToOneField(User, related_name="vendor", on_delete=models.CASCADE)
+
     name = models.CharField(max_length=100, help_text="Shop Name", unique=True)
+
     slug = models.SlugField(unique=True, max_length=200)
+
     description = models.TextField(blank=True, null=True)
+
     mobile = models.CharField(
         max_length=100, help_text="Shop Mobile Number", null=True, blank=True
     )
+
     image = models.ImageField(
         upload_to="vendors", blank=True, null=True, default="vendor.jpg"
     )
+
     active = models.BooleanField(default=False)
+
     datetime_created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
@@ -33,4 +41,3 @@ class Vendor(models.Model):
         if self.slug == "" or self.slug is None:
             self.slug = slugify(self.name)
         super(Vendor, self).save(*args, **kwargs)
-
