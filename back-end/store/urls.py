29d80d827fcaced17ebaa7bb6d75390037680bfb
R.B <110852196+MRFAKE2002@users.noreply.github.com>
2024-11-11 19:05:01 +0330

    Make Product and Category list view and its urls

---
+++ back-end/store/urls.py
@@ -0,0 +1,11 @@
+# Django
+from django.urls import path
+
+# My apps
+from . import views as store_views
+
+
+urlpatterns = [
+    path("category/", store_views.CategoryListAPIView.as_view(), name="categories_list"),
+    path("product/", store_views.ProductListAPIView.as_view(), name="products_list"),
+]
--- back-end/store/views.py
+++ back-end/store/views.py
@@ -1,3 +1,26 @@
+# Django
 from django.shortcuts import render
 
-# Create your views here.
+# Libraries
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+
+# My apps
+from .models import Category, Product
+from .serializers import CategorySerializer, ProductSerializer
+
+
+# dar inja ma miaim 'data' marbut be 'Product' ro be surat 'list' namayesh midim.
+class ProductListAPIView(generics.ListAPIView):
+  queryset = Product.objects.all()
+  serializer_class= ProductSerializer
+  permission_classes = [AllowAny]
+
+
+# dar inja ma miaim 'data' marbut be 'Category' ro be surat 'list' namayesh midim.
+class CategoryListAPIView(generics.ListAPIView):
+  queryset = Category.objects.all()
+  serializer_class= CategorySerializer
+  permission_classes = [AllowAny]
+
+
