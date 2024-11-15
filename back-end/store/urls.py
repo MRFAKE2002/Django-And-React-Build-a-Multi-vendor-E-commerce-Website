7cba9af49d5af013ff9885f8cf189fc674fe82a3
R.B <110852196+MRFAKE2002@users.noreply.github.com>
2024-11-15 12:46:04 +0330

    Make CartListCreateAPIView and its url

--- back-end/store/urls.py
+++ back-end/store/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("categories/", store_views.CategoryListAPIView.as_view(), name="categories_list"),
     path("products/", store_views.ProductListAPIView.as_view(), name="products_list"),
     path("product/<slug>/", store_views.ProductDetailAPIView.as_view(), name="products_detail"),
+    path("cart/", store_views.CartListCreateAPIView.as_view(), name="cart_list_create"),
 ]
--- back-end/store/views.py
+++ back-end/store/views.py
@@ -1,14 +1,15 @@
 # Django
-from django.shortcuts import render
 
 # Libraries
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from decimal import Decimal
 
 # My apps
-from .models import Category, Product
-from .serializers import CategorySerializer, ProductSerializer
-
+from .models import Category, Product, Cart, Tax
+from .serializers import CategorySerializer, ProductSerializer, CartSerializer
+from userauths.models import User
 
 # dar inja ma miaim 'data' marbut be 'Product' ro be surat 'list' namayesh midim.
 class ProductListAPIView(generics.ListAPIView):
@@ -35,4 +36,88 @@ class ProductDetailAPIView(generics.RetrieveAPIView):
     slug = self.kwargs["slug"]
     
     return Product.objects.get(slug=slug)
+
+
+class CartListCreateAPIView(generics.ListCreateAPIView):
+  """
+    dakhel in 'class' ma mikhaim 'object Cart' besazim va ham betunim 'List object Cart' ro bebinim.
+  """
+  queryset = Cart.objects.all()
+  serializer_class = CartSerializer
+  permission_classes = [AllowAny]
+
+  def create(self, request, *args, **kwargs):
+    # dar inja oun data ke az samt 'Front' miad ro migirim ta betunim 'object Cart' besazim.
+
+    # inja oun data ro az 'Front' migirim.
+    payload = request.data
+
+    product_id = payload['product_id']
+    user_id = payload['user_id']
+    quantity = payload['quantity']
+    price = payload['price']
+    shipping_amount = payload['shipping_amount']
+    country = payload['country']
+    country = payload['country']
+    color = payload['color']
+    size = payload['size']
+    cart_id = payload['cart_id']
+
+    product = Product.objects.get(id=product_id)
+
+    #? if user_id != "undefined":
+    #?   user = User.objects.get(id=user_id)
+    #? else:
+    #?   user = None
+    user = User.objects.get(id=user_id) if user_id != "undefined" else None
+
+    #? tax = Tax.objects.filter(country=country).first()
+    if tax := Tax.objects.filter(country=country).first():
+      tax_rate = tax.rate / 100
+    else:
+      tax_rate = 0
+
+    service_fee_percentage = 20 / 100
+    if cart := Cart.objects.filter(cart_id=cart_id, product=product).first():
+      cart.product = product
+      cart.user = user
+      cart.quantity = quantity
+      cart.price = price
+      cart.sub_total = Decimal(price) * int(quantity)
+      cart.shipping_amount = Decimal(shipping_amount) * int(quantity)
+      cart.tax_fee = Decimal(tax_rate) * int(quantity)
+      cart.color = color
+      cart.size = size
+      cart.country = country
+      cart.cart_id = cart_id
+
+      cart.service_fee = Decimal(service_fee_percentage) * Decimal(cart.sub_total)
+
+      cart.total = cart.sub_total + cart.shipping_amount + cart.service_fee + cart.tax_fee
+
+      cart.save()
+
+      return Response({"message": "Cart Updated Successfully"}, status = status.HTTP_200_OK)
+    else:
+      cart = Cart()
+
+      cart.product = product
+      cart.user = user
+      cart.quantity = quantity
+      cart.price = price
+      cart.sub_total = Decimal(price) * int(quantity)
+      cart.shipping_amount = Decimal(shipping_amount) * int(quantity)
+      cart.tax_fee = Decimal(tax_rate) * int(quantity)
+      cart.color = color
+      cart.size = size
+      cart.country = country
+      cart.cart_id = cart_id
+
+      cart.service_fee = Decimal(service_fee_percentage) * Decimal(cart.sub_total)
+
+      cart.total = cart.sub_total + cart.shipping_amount + cart.service_fee + cart.tax_fee
+
+      cart.save()
+
+      return Response({"message": "Cart Updated Successfully"}, status = status.HTTP_201_CREATED)
 
