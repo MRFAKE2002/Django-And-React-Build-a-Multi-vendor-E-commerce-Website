b81e85159d4e6b4f96d8ca976b9b1596f0253b30
R.B <110852196+MRFAKE2002@users.noreply.github.com>
2025-02-07 23:25:06 +0330

    Add functions to get product color size and quantity

--- front-end/src/views/store/Product.jsx
+++ front-end/src/views/store/Product.jsx
@@ -6,8 +6,132 @@ import { Link } from "react-router-dom";
 import axiosAPIInstance from "../../utils/axios";
 
 function Product() {
+  //! Custom States
+
   const [products, setProducts] = useState([]);
   const [categories, setCategories] = useState([]);
+
+  /*
+    ma inja mikhaim meghdar 'Quantity Color Size' ro ke 'user' baraye 'product' moshakhas mikone va 
+    mikhad be 'cart' ezafe kone ro miaim inja dakhel 'state inputsValues' gharar midim.
+    hala age 'user' bezane 'add to cart' ma miaim in data ro be 'Back-end' midim ta 'cart' besaze.
+  */
+  const [productValues, setProductValues] = useState({});
+
+  //! Custom Functions
+
+  //? const handleProductValuesButtonClick = (event) => {
+  //?   /*
+  //!     'logic in code' baraye zamani ke bekhaim az yek 'product' betune chand ta 'size va color' entekhab bokone
+  //?     dar in function ma mikhaim 'data' male 'product' ro ke 'user' mikhad be 'cart' befreste, begirim va dakhel yek 'state'
+  //?     gharar bedim. hala miaim az 'button' oun 'productID va oun name va value' ro migirim ta dakhel 'state' bezarim.
+  //?     inja dalil estefade az 'currentTarget' bejaye 'target' ine ke momkene dakhel 'button' yek 'tag' dige mesl 'span'
+  //?     vojud dashte bashe va 'event.target' momkene be oun 'tag' dakhel 'button' eshare kone baraye hamin miaim 'currentTarget'
+  //?     mizanim ke faghat be 'button' eshare kone.
+  //?   */
+  //?   const productID = event.currentTarget.getAttribute("data-product-id");
+
+  //?   // inja momkene 'name' meghdar 'color ya size ya quantity' bashe.
+  //?   const name = event.currentTarget.name;
+
+  //?   // inja mishe oun meghdar entekhab shode.
+  //?   const value = event.currentTarget.innerHTML;
+
+  //?   // gereftan 'color code' az 'style button'
+  //?  const colorCode = event.currentTarget.style.backgroundColor;
+
+  //?   // age productID vojud nadasht kari nakone.
+  //?   if (!productID) return;
+
+  //?   setProductValues((prevValues) => ({
+  //?     ...prevValues,
+  //?     [productID]: {
+
+  //?       // 'data' ghabli ke dakhelesh bud
+  //?       ...prevValues[productID],
+
+  //?       // migim age oun 'name' mesl 'size ya color ya quantity' dakhel 'data' in 'productID' hast ya na.
+  //?       [name]: prevValues[productID]?.[name]
+
+  //?         /*
+  //?         agar 'bud' ke bia aval 'data' ghabli ro gharar bede badesh age 'name' barabar 'color' bud bia 'colorCode' ro
+  //?        bede age 'size' bud bia meghdar 'value' ro bezar
+  //?         */
+  //?         ? [...prevValues[productID][name], name === "color" ? colorCode : value]
+
+  //?         // age aslan 'meghdari' az ghabl dakhel in 'name' nabud bia meghdar jadid bede.
+  //?         : [name === "color" ? colorCode : value],
+  //?     },
+  //?   }));
+  //? };
+  //? console.log(productValues);
+  //? /*
+  //?   "1": {
+  //?     "size": ["XL", "L"],
+  //?     "color": ["red", "blue"]
+  //?   },
+  //?   "2": {
+  //?     "size": ["M"],
+  //?     "color": ["green", "yellow"]
+  //?   }
+  //? }
+  //? */
+
+  const handleProductValuesButtonClick = (event) => {
+    /*
+      dar in function ma mikhaim 'data' male 'product' ro ke 'user' mikhad be 'cart' befreste, begirim va dakhel yek 'state'
+      gharar bedim. hala miaim az 'button' oun 'productID va oun name va value' ro migirim ta dakhel 'state' bezarim.
+      inja dalil estefade az 'currentTarget' bejaye 'target' ine ke momkene dakhel 'button' yek 'tag' dige mesl 'span'
+      vojud dashte bashe va 'event.target' momkene be oun 'tag' dakhel 'button' eshare kone baraye hamin miaim 'currentTarget'
+      mizanim ke faghat be 'button' eshare kone.
+     */
+    const productID = event.currentTarget.getAttribute("data-product-id");
+    const name = event.currentTarget.name; // گرفتن نام (مثل color یا size)
+
+    if (!productID) return;
+
+    const value =
+      name === "color"
+        ? event.currentTarget.getAttribute("data-color-name")
+        : event.currentTarget.innerHTML;
+
+    setProductValues((prevValues) => ({
+      ...prevValues,
+      [productID]: {
+        ...prevValues[productID],
+        [name]: value,
+      },
+    }));
+  };
+
+  // Handle quantity change from input
+  const handleProductQuantityOnChange = (event) => {
+    const productID = event.currentTarget.getAttribute("data-product-id");
+    const value = event.target.value;
+
+    // Update the quantity in state for the specific product
+    setProductValues((prevValues) => ({
+      ...prevValues,
+      [productID]: {
+        ...prevValues[productID], // Preserve previous values of the product
+        quantity: value, // Update quantity
+      },
+    }));
+  };
+
+  console.log(productValues);
+  /*
+    {
+      "1": {
+        "size": "XL",
+        "color": "rgb(255, 0, 0)"
+      },
+      "2": {
+        "size": "M",
+        "color": "rgb(0, 128, 0)"
+      }
+    }
+  */
 
   useEffect(() => {
     axiosAPIInstance.get("products/").then((response) => {
@@ -22,6 +146,8 @@ function Product() {
       setCategories(response.data);
     });
   }, []);
+
+  //! JSX
 
   return (
     <>
@@ -69,7 +195,10 @@ function Product() {
                       </a>
                     </div>
                     <div className="card-body">
-                      <Link to={`detail/${product.slug}/`} className="text-reset">
+                      <Link
+                        to={`detail/${product.slug}/`}
+                        className="text-reset"
+                      >
                         <h5 className="card-title mb-3">{product.name}</h5>
                       </Link>
                       <a href="" className="text-reset">
@@ -100,49 +229,65 @@ function Product() {
                             <li className="p-1">
                               <b>Size</b>: XL
                             </li>
-                            <div className="p-1 mt-0 pt-0 d-flex flex-wrap">
-                              <li>
-                                <button className="btn btn-secondary btn-sm me-2 mb-1">
-                                  XXL
-                                </button>
-                              </li>
-                              <li>
-                                <button className="btn btn-secondary btn-sm me-2 mb-1">
-                                  XXL
-                                </button>
-                              </li>
-                              <li>
-                                <button className="btn btn-secondary btn-sm me-2 mb-1">
-                                  XXL
-                                </button>
-                              </li>
-                            </div>
-                          </div>
-                          <div className="d-flex flex-column mt-3">
-                            <li className="p-1">
-                              <b>COlor</b>: Red
-                            </li>
-                            <div className="p-1 mt-0 pt-0 d-flex flex-wrap">
-                              <li>
-                                <button
-                                  className="btn btn-sm me-2 mb-1 p-3"
-                                  style={{ backgroundColor: "red" }}
-                                />
-                              </li>
-                              <li>
-                                <button
-                                  className="btn btn-sm me-2 mb-1 p-3"
-                                  style={{ backgroundColor: "green" }}
-                                />
-                              </li>
-                              <li>
-                                <button
-                                  className="btn btn-sm me-2 mb-1 p-3"
-                                  style={{ backgroundColor: "yellow" }}
+                            <div className="p-1 mt-0 pt-0 d-block">
+                              <li key={index}>
+                                <input
+                                  type="number"
+                                  data-product-id={product.id}
+                                  name="quantity"
+                                  min="1"
+                                  max={product.stock_quantity}
+                                  value={productValues[product.id]?.quantity || 1}
+                                  className="form-control block"
+                                  onChange={handleProductQuantityOnChange}
                                 />
                               </li>
                             </div>
                           </div>
+                          {product.sizes?.length > 0 && (
+                            <div className="d-flex flex-column">
+                              <li className="p-1">
+                                <b>Size</b>: XL
+                              </li>
+                              <div className="p-1 mt-0 pt-0 d-flex flex-wrap">
+                                {product.sizes?.map((size, index) => (
+                                  <li key={index}>
+                                    <button
+                                      name="size"
+                                      data-product-id={product.id}
+                                      className="btn btn-secondary btn-sm me-2 mb-1"
+                                      onClick={handleProductValuesButtonClick}
+                                    >
+                                      {size.name}
+                                    </button>
+                                  </li>
+                                ))}
+                              </div>
+                            </div>
+                          )}
+                          {product.colors?.length > 0 && (
+                            <div className="d-flex flex-column mt-3">
+                              <li className="p-1">
+                                <b>COlor</b>: Red
+                              </li>
+                              <div className="p-1 mt-0 pt-0 d-flex flex-wrap">
+                                {product.colors?.map((color, index) => (
+                                  <li key={index}>
+                                    <button
+                                      className="btn btn-sm me-2 mb-1 p-3"
+                                      name="color"
+                                      data-color-name={color.name}
+                                      data-product-id={product.id}
+                                      style={{
+                                        backgroundColor: `${color.color_code}`,
+                                      }}
+                                      onClick={handleProductValuesButtonClick}
+                                    />
+                                  </li>
+                                ))}
+                              </div>
+                            </div>
+                          )}
                           <div className="d-flex mt-3 p-1">
                             <button
                               type="button"
