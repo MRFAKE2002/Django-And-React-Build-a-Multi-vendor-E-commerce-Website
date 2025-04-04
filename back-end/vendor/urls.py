bab675db72239271f03d11f3d944f4fe96507184
R.B <110852196+MRFAKE2002@users.noreply.github.com>
2025-04-05 01:38:45 +0330

    Add function monthly chart for orders and products

--- back-end/vendor/urls.py
+++ back-end/vendor/urls.py
@@ -11,5 +11,16 @@ urlpatterns = [
         "stats/<vendor_id>/",
         vendor_views.SummaryStatsAPIView.as_view(),
         name="vendor_stats",
+    ),
+    #! Order & Product | Monthly Chart 
+    path(
+        "vendor-orders-chart/<vendor_id>/",
+        vendor_views.MonthlyOrdersChartAPIView,
+        name="monthly_orders_chart",
+    ),
+    path(
+        "vendor-products-chart/<vendor_id>/",
+        vendor_views.MonthlyProductsChartAPIView,
+        name="monthly_products_chart",
     )
 ]
--- back-end/vendor/views.py
+++ back-end/vendor/views.py
@@ -1,11 +1,13 @@
 # Django
 from django.db.models import F, Sum
+from django.db.models.functions import ExtractMonth
 
 # Libraries
 from rest_framework import generics, status
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError, NotFound
+from rest_framework.decorators import api_view
 
 # My Apps
 from .serializers import SummarySerializers
@@ -13,6 +15,7 @@ from .models import Vendor
 from store.models import Order, OrderItem, Product
 
 # ---------------------------------------------Stats List View ------------------------------------------ #
+
 
 class SummaryStatsAPIView(generics.ListAPIView):
     serializer_class = SummarySerializers
@@ -46,6 +49,60 @@ class SummaryStatsAPIView(generics.ListAPIView):
     def list(self):
         queryset = self.get_queryset()
         serializer = self.get_serializer(queryset, many=True)
-        
+
         return Response(serializer.data)
 
+
+# ---------------------------------------------Order Product Monthly Chart View ------------------------------------------ #
+
+
+@api_view(["GET"])
+def MonthlyOrdersChartAPIView(request, vendor_id):
+    if not vendor_id or vendor_id == "undefined":
+        raise ValidationError({"message": "Vendor ID is missing."})
+
+    try:
+        vendor = Vendor.objects.get(id=vendor_id)
+    except Vendor.DoesNotExist as e:
+        raise NotFound({"message": "Vendor not found."}) from e
+
+    try:
+        orders = Order.objects.select_related("vendor").filter(
+            vendor=vendor, payment_status="paid"
+        )
+    except Order.DoesNotExist as e:
+        raise NotFound({"message": "Order not found."}) from e
+
+    orders_by_months = (
+        orders.annotate(month=ExtractMonth("date"))
+        .values("month")
+        .annotate(orders=Sum("id"))
+        .order_by("month")
+    )
+
+    return Response(orders_by_months)
+
+
+@api_view(["GET"])
+def MonthlyProductsChartAPIView(request, vendor_id):
+    if not vendor_id or vendor_id == "undefined":
+        raise ValidationError({"message": "Vendor ID is missing."})
+
+    try:
+        vendor = Vendor.objects.get(id=vendor_id)
+    except Vendor.DoesNotExist as e:
+        raise NotFound({"message": "Vendor not found."}) from e
+
+    try:
+        products = Product.objects.select_related("vendor").filter(vendor=vendor)
+    except Product.DoesNotExist as e:
+        raise NotFound({"message": "Product not found."}) from e
+
+    products_by_months = (
+        products.annotate(month=ExtractMonth("date"))
+        .values("month")
+        .annotate(orders=Sum("id"))
+        .order_by("month")
+    )
+
+    return Response(products_by_months)
